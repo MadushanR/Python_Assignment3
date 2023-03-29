7d5416a54957e06e587e599e773e022ba1861036
Rajendran Madushan <madushanrajendran@gmail.com>
2023-03-29 11:21:57 -0400

    added exception handling

--- Bank.py
+++ Bank.py
@@ -25,27 +25,59 @@ class Bank():
             accountList.append(account)
 
     def openAccounts():        
-        accountType = int(input("Enter account type \n 1.Savings Account \n 2.Chequing Account\n "))
-        if accountType == 1:
-            accountNumber = random.randrange(1,1000)
-            accountHolderName = input("\nEnter account holder name ")
-            currentBalance = float(input("Enter current balance "))  
-            minimumBalance = float(input("Enter the minimum balance "))
-            account = SavingsAccount.SavingsAccount(accountNumber,accountHolderName,currentBalance,minimumBalance)
-            accountList.append(account)
-            print("\nAccount created ")
-        elif accountType == 2:
-            accountNumber = random.randrange(1,1000)
-            accountHolderName = input("\nEnter account holder name ")
-            currentBalance = float(input("Enter current balance "))  
-            overDraft = float(input("Enter the overdraft amount "))
-            account = ChequingAccount.ChequingAccount(accountNumber,accountHolderName,currentBalance,overDraft)
-            accountList.append(account)
-            print("\nAccount created ")
-        else:
-             print("\nEnter a valid choice\n")
-             Bank.openAccounts()
-        Bank.display()
+        try:
+         accountType = int(input("Enter account type \n 1.Savings Account \n 2.Chequing Account\n "))
+         if accountType == 1:
+             accountNumber = random.randrange(1,1000)
+             accountHolderName =  input("\nEnter account holder name ")
+             try:
+                 currentBalance = float(input("Enter current balance ")) 
+                 while currentBalance < 0:
+                     currentBalance = float(input("Enter a valid value "))
+             except:
+                     currentBalance = float(input("Enter a valid value "))
+                     while currentBalance < 0:
+                         currentBalance = float(input("Enter a valid value"))
+             try:
+                 minimumBalance = float(input("Enter the minimum balance "))
+                 while minimumBalance < 0:
+                     minimumBalance = float(input("Enter a valid value "))
+             except:
+                     minimumBalance = float(input("Enter a valid value "))
+                     while minimumBalance < 0:
+                         minimumBalance = float(input("Enter a valid value"))
+             account = SavingsAccount.SavingsAccount(accountNumber,accountHolderName,currentBalance,minimumBalance)
+             accountList.append(account)
+             print("\nAccount created ")
+         elif accountType == 2:
+             accountNumber = random.randrange(1,1000)
+             accountHolderName = input("\nEnter account holder name ")
+             try:
+                 currentBalance = float(input("Enter current balance ")) 
+                 while currentBalance <= 0:
+                     currentBalance = float(input("Enter a valid value "))
+             except:
+                     currentBalance = float(input("Enter a valid value "))
+                     while currentBalance <= 0:
+                         currentBalance = float(input("Enter a valid value"))            
+             try:
+                 overDraft = float(input("Enter the overdraft amount "))
+                 while overDraft <= 0:
+                     overDraft = float(input("Enter a valid value "))
+             except:
+                     overDraft = float(input("Enter a valid value "))
+                     while overDraft <= 0:
+                         overDraft = float(input("Enter a valid value"))
+             account = ChequingAccount.ChequingAccount(accountNumber,accountHolderName,currentBalance,overDraft)
+             accountList.append(account)
+             print("\nAccount created ")
+         else:
+              print("\nEnter a valid choice\n")
+              Bank.openAccounts()
+              Bank.display()
+        except:
+            print("\nEnter a valid choice\n")
+            Bank.openAccounts()  
 
     def display():
         print("\nAccount Number    |Account Holder Name|Current Balance|Account Type")
@@ -56,33 +88,43 @@ class Bank():
         found = False
         for i in accountList:
             if i.accountNumber == accountNumber :
-                print("Accunt number is : ",i.accountNumber," Account holder name is : ",i.accountHolderName, " and current balance is : ",i.currentBalance)
+                print("Account number is : ",i.accountNumber," Account holder name is : ",i.accountHolderName, " and current balance is : ",i.currentBalance)
                 found = True
         if found == False:
                 print("Invalid Account")
 
     def deposit(accountNumber):
-        found = False
-        for i in accountList:
-            if i.accountNumber == accountNumber :
-                account = Bank(i.accountNumber,i.accountHolderName,i.currentBalance)
-                amount = float(input("Enter the amount "))
-                Account.Account.deposit(i,amount)
-                print("Transaction successful and current balance is : ",i.currentBalance)
-                found = True
-        if found == False:
-                print("Invalid Account")
+        try:
+         found = False
+         for i in accountList:
+             if i.accountNumber == accountNumber :
+                 account = Bank(i.accountNumber,i.accountHolderName,i.currentBalance)
+                 amount = float(input("Enter the amount "))
+                 while amount <= 0:
+                    amount = float(input("Enter valid amount "))
+                 Account.Account.deposit(i,amount)
+                 print("Transaction successful and current balance is : ",i.currentBalance)
+                 found = True
+         if found == False:
+                 print("Invalid Account")
+        except :
+            print("Going back to main menu due to invalid input")
 
 
     def withdraw(accountNumber):
-        found = False
-        for i in accountList:
-            if i.accountNumber == accountNumber :
-                amount = float(input("Enter the amount "))
-                if i.accountType == "Savings Account":
-                    SavingsAccount.SavingsAccount.withdraw(i,amount)
-                else:
-                    ChequingAccount.ChequingAccount.withdraw(i,amount)
-                found = True
-        if found == False:
-                print("Invalid Account")
+        try:
+         found = False
+         for i in accountList:
+             if i.accountNumber == accountNumber :
+                 amount = float(input("Enter the amount "))
+                 while amount <= 0:
+                    amount = float(input("Enter valid amount "))
+                 if i.accountType == "Savings Account":
+                     SavingsAccount.SavingsAccount.withdraw(i,amount)
+                 else:
+                     ChequingAccount.ChequingAccount.withdraw(i,amount)
+                 found = True
+         if found == False:
+                 print("Invalid Account")
+        except :
+            print("Going back to main menu due to invalid input")
--- Program.py
+++ Program.py
@@ -5,49 +5,76 @@ class Program():
         Bank.Bank.generateAccounts()
 
 def showAccountMenu():
-    print("\nEnter 1 to check balance ")
-    print("Enter 2 to deposit ")
-    print("Enter 3 to withdraw ")
-    print("Enter 4 to go back to main menu ")
-    print("Enter 5 to see all accounts ")
-    choice = int(input("\nEnter the choice "))
-    if choice == 1 :
-        accountNumber = int(input("Enter account number "))
-        Bank.Bank.searchAccount(accountNumber)
-        showAccountMenu()
-    elif choice == 2:
-        accountNumber = int(input("Enter account number "))
-        Bank.Bank.deposit(accountNumber)
-        showAccountMenu()
-    elif choice == 3:
-        accountNumber = int(input("Enter account number "))
-        Bank.Bank.withdraw(accountNumber)
-        showAccountMenu()
-    elif choice == 4:
-        showMainMenu()
-    elif choice == 5:
-        Bank.Bank.display()
-        showAccountMenu()
-    else:
+    try:
+     print("\nEnter 1 to check balance ")
+     print("Enter 2 to deposit ")
+     print("Enter 3 to withdraw ")
+     print("Enter 4 to go back to main menu ")
+     print("Enter 5 to see all accounts ")
+     choice = int(input("\nEnter the choice "))
+     if choice == 1 :
+        try:
+          accountNumber = int(input("Enter account number "))
+          while accountNumber <= 0:
+             accountNumber = int(input("Enter valid account number "))
+          Bank.Bank.searchAccount(accountNumber)
+          showAccountMenu()
+        except:
+         print("\nEnter a valid input")
+         showAccountMenu()
+     elif choice == 2:
+         try:
+             accountNumber = int(input("Enter account number "))
+             while accountNumber <= 0:
+              accountNumber = int(input("Enter valid account number "))
+             Bank.Bank.deposit(accountNumber)
+             showAccountMenu()
+         except:
+          print("\nEnter a valid input")
+          showAccountMenu()
+     elif choice == 3:
+         try:
+             accountNumber = int(input("Enter account number "))
+             while accountNumber <= 0:
+              accountNumber = int(input("Enter valid account number "))
+             Bank.Bank.withdraw(accountNumber)
+             showAccountMenu()
+         except:
+          print("\nEnter a valid input")         
+          showAccountMenu()
+     elif choice == 4:
+         showMainMenu()
+     elif choice == 5:
+         Bank.Bank.display()
+         showAccountMenu()
+     else:
+         print("\nEnter a valid choice")
+         showAccountMenu()
+    except:
         print("\nEnter a valid choice")
         showAccountMenu()
 
 
+
 def showMainMenu():
-    print("\nEnter 1 to open an account ")
-    print("Enter 2 to select an account ")
-    print("Enter 3 to exit \n")
-    choice = int(input("Enter the choice "))
-    if choice == 1:
-        Bank.Bank.openAccounts()
-        showMainMenu()
-    elif choice == 2:
-        showAccountMenu()
-    elif choice == 3:
-        print("\nThank you")
-    else:
+    try:
+     print("\nEnter 1 to open an account ")
+     print("Enter 2 to select an account ")
+     print("Enter 3 to exit \n")
+     choice = int(input("Enter the choice "))
+     if choice == 1:
+         Bank.Bank.openAccounts()
+         showMainMenu()
+     elif choice == 2:
+         showAccountMenu()
+     elif choice == 3:
+         print("\nThank you")
+     else:
+         print("\nEnter a valid choice ")
+         choice = int(input("Enter the choice "))
+         showMainMenu()
+    except:
         print("\nEnter a valid choice ")
-        choice = int(input("Enter the choice "))
         showMainMenu()
 
 showMainMenu()
